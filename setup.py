809c127bcc27af22bc7600258507fda3eb88318c
Rodrigo <ro.goab@gmail.com>
2021-01-29 02:45:29 -0300

    se recompila

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import find_packages, setup
-setup(
+import setuptools
+
+setuptools.setup(
     name='openimagesdatasetutils',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     version='0.0.1',
     description='Utils para trabajar con openimagedataset',
     author='Rodrigo',
