c78eb3ff8ba4d48bc8c7df9327d7a0974578b68a
Rodrigo <ro.goab@gmail.com>
2021-01-29 02:52:37 -0300

    se recompila

--- openimagesdatasetutils/dataset_utils.py
+++ openimagesdatasetutils/dataset_utils.py
@@ -22,6 +22,7 @@ import pandas as pd
 
 
 def create_category_index(categories):
+    category_index=[]
     num_categories = 0
     #se crea un indice de categorias con id y nombre
     for category in categories:
